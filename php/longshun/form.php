780abd7db38a34471225b796fc8f5735255d7835
lumanman21 <3171995302@qq.com>
2024-10-31 16:05:19 +0800

    lz

--- php/longshun/form.php
+++
@@ -1,7 +0,0 @@
-<section class="com_fom br p80">
-	<div class="wrap">
-		<i></i>
-		<h6>We Look Forward To Reaching Caooperation With You</h6>
-		<?php echo do_shortcode( '[wpforms id="26"]' ); ?>
-	</div>
-</section>
